e9a7c0d2503340328fe63ce5642be2dd81c46846
Jerry <6jerrylu@gmail.com>
2021-09-06 23:30:56 -0400

    Organize project file structure

--- BackEnd/ethereum/utils/compile.js
+++ BackEnd/ethereum/utils/compile.js
@@ -9,17 +9,38 @@ fs.removeSync(buildpath);
 
 
 // Fetch source files and compile
-const filepath = path.resolve(__dirname, '../contracts', 'user_database.sol');
+const filepath = path.resolve(__dirname, '../contracts', 'login_database.sol');
 const source = fs.readFileSync(filepath, 'utf-8');
-const compiledContracts = solc.compile(source, 1).contracts;
 
+const compile_settings = {
+    language: 'Solidity',
+    sources: {
+      filepath: {
+        content: source
+      }
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['metadata', 'abi', 'evm.bytecode']
+        }
+      }
+    }
+  };
+
+const compiledContracts = JSON.parse(solc.compile(JSON.stringify(compile_settings))).contracts.filepath;
 
 // Create new build directory
 fs.ensureDirSync(buildpath);
 
 for (let contract in compiledContracts) {
     fs.outputJsonSync(
-        path.resolve(buildpath, contract.slice(1) + ".json"),
-        compiledContracts[contract]
+        path.resolve(buildpath, contract + ".json"),
+        {
+            "matadata": compiledContracts[contract]["metadata"],
+            "abi": compiledContracts[contract]["abi"],
+            "bytecode": compiledContracts[contract]["evm"]["bytecode"]["object"]
+        }
     );
+    console.log("Contract " + contract + " compiled successfully and stored to " + path.resolve(buildpath, contract + ".json"))
 }
