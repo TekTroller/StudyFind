40d623717a32c24446f58daf664180b649db7286
Jerry <6jerrylu@gmail.com>
2021-10-01 22:24:47 -0400

    Google firestore backend api

--- BackEnd/firestore/storageClient.js
+++ BackEnd/firestore/storageClient.js
@@ -8,32 +8,34 @@ const WalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 
 class StorageClient {
-    constructor(patientAddress) {
+    constructor() {
         // Firestore
         this.client = new Firestore({
             projectId: 'studyfind-medical-database',
             keyFilename: path.join(__dirname, 'keys/studyfind-medical-database-key.json')
         });
+    }
 
+    async set_patient(patientAddress) {
         // Ethereum contract
         const mnemonic = 'stairs basic flag mandate split marble oven cliff anxiety trap model morning';
         const infura_provider = 'https://rinkeby.infura.io/v3/124660071809414c801dce8cc1941cae';
         const provider = new WalletProvider(mnemonic, infura_provider);
         const web3 = new Web3(provider);
         this.patient = new web3.eth.Contract(Patient.abi, patientAddress);
-        this.account = "";
-        web3.eth.getAccounts().then(accounts => {
-            this.account = accounts[0];
-        });
+        const accounts = await web3.eth.getAccounts();
+        this.account = accounts[0];
     }
 
     async upload(filename, file) {
         // Compute a token for file
         const file_token = sha256(file);
-    
+
         // Upload file to google firestore
         const doc_ref = this.client.collection('root').doc(file_token);
-        await doc_ref.set(file);
+        await doc_ref.set({
+            data:file
+        });
 
         // Upload file-token pair to Ethereum network
         try {
@@ -52,15 +54,15 @@ class StorageClient {
         try {
             file_token = await this.patient.methods.view_file(filename).call();
         } catch (err) {
-            throw new Error("File not exists");
+            throw err;
         }
 
         // Retrieve file from firestore
-        const doc_ref = this.client.collection('root').doc(file_token);
-        const file = doc_ref.get();
-        
-        return file;
+        const doc_ref = this.client.doc("root/" + file_token);
+        const file = await doc_ref.get();
+
+        return file.data();
     }
 }
 
-module.exports = new StorageClient();
+module.exports = StorageClient;
